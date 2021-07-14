ba4788acf1477cdce862abe276720df4b605d9f1
leen-x <linsongxiong2015@163.com>
2021-07-14 14:11:39 +0800

    [UPD] Threadlocal、InheritableThreadLocal、TransmittableThreadLocal对比

--- src/main/java/com/leenx/learn/mavenproject/TransmittableThreadLocalDemo.java
+++ src/main/java/com/leenx/learn/mavenproject/TransmittableThreadLocalDemo.java
@@ -3,6 +3,7 @@ package com.leenx.learn.mavenproject;
 import com.alibaba.ttl.TransmittableThreadLocal;
 import com.alibaba.ttl.TtlRunnable;
 
+import java.text.MessageFormat;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -14,36 +15,43 @@ import java.util.concurrent.TimeUnit;
  **/
 public class TransmittableThreadLocalDemo {
     public static class DemoUtil {
-        private static final TransmittableThreadLocal<String> threadName = new TransmittableThreadLocal<>();
+        private static final ThreadLocal<String> threadLocal = new ThreadLocal<>();
+        private static final InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+        private static final TransmittableThreadLocal<String> transmittableThreadLocal = new TransmittableThreadLocal<>();
 
-        public static String get() {
-            return threadName.get();
+
+        public static void get() {
+            String format = "ThreadLocal:{0}  InheritableThreadLocal:{1} TransmittableThreadLocal:{2}";
+            System.out.println(MessageFormat.format(format, threadLocal.get(), inheritableThreadLocal.get(), transmittableThreadLocal.get()));
         }
 
         public static void put(String value) {
-            threadName.set(value);
+            threadLocal.set(value);
+            inheritableThreadLocal.set(value);
+            transmittableThreadLocal.set(value);
         }
     }
 
     public static class MyRunnable implements Runnable {
         @Override
         public void run() {
-            System.out.println(Thread.currentThread().getName() + " get from threadLocal : " + DemoUtil.get());
+            DemoUtil.get();
         }
     }
 
     public static void main(String[] args) {
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(1, 20,60, TimeUnit.SECONDS, new ArrayBlockingQueue(1024));
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(1, 20, 60, TimeUnit.SECONDS, new ArrayBlockingQueue(1024));
         Runnable commonRunnable = new MyRunnable();
         /**
-         * value_1
+         * ThreadLocal value_1
+         * 创建线程是继承
          */
         DemoUtil.put("value_1");
-        System.out.println("【main主线程】" + DemoUtil.get());
+        System.out.println("【main主线程】value_1");
         pool.submit(commonRunnable);
 
         /**
-         * value_2
+         * ThreadLocal value_2
          */
         try {
             TimeUnit.SECONDS.sleep(1);
@@ -53,10 +61,12 @@ public class TransmittableThreadLocalDemo {
         }
 
         DemoUtil.put("value_2");
-        System.out.println("【main主线程】" + DemoUtil.get());
+        System.out.println("【main主线程】value_2");
+        // Runnable
+        pool.submit(commonRunnable);
+        // TtlRunnable
         TtlRunnable ttlRunnable = TtlRunnable.get(commonRunnable);
         pool.submit(ttlRunnable);
-        pool.submit(commonRunnable);
 
         try {
             TimeUnit.SECONDS.sleep(100);
